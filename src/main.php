d7e6b72d34c49caa1158684bdafd0ba5a5ca9ed5
Sebastian <donalffons@gmail.com>
2018-08-16 16:53:43 +0200

    include i2ConfiguratorCore builds

--- src/main.php
+++ src/main.php
@@ -243,7 +243,8 @@ f00bar;
 				<title>I2 Configurator - Explorer</title>
 				<meta charset="utf-8">
 				<meta http-equiv="X-UA-Compatible" content="IE=edge">
-				<meta name="viewport" content="width=device-width, initial-scale=1">';
+				<meta name="viewport" content="width=device-width, initial-scale=1">
+				<script src="i2ConfiguratorCore/build/i2ConfiguratorCore.js"></script>';
 		$this->getCSS();
 		print '</head><body>';
 	}
